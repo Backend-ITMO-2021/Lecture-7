debf22ecd6e39d84120cd7a3391926f399ac6858
Ilya Halsky <ilya.kachalsky@gmail.com>
2021-04-24 19:20:37 +0300

    fix types

--- src/main/scala/ru/ifmo/backend_2021/Parser.scala
+++ src/main/scala/ru/ifmo/backend_2021/Parser.scala
@@ -8,12 +8,12 @@ object Parser {
   val alternative: Alternative[Parser] = ???
   val monad: Monad[Parser] = ???
 
-  val ok: Parser[()] = ???
-  val eof: Parser[()] = ???
+  val ok: Parser[Unit] = ???
+  val eof: Parser[Unit] = ???
   val satisfy: (Char => Boolean) => Parser[Char] = ???
   val element: Char => Parser[Char] = ???
   val stream: String => Parser[String] = ???
 
-  val brackets: Parser[()] = ???
+  val brackets: Parser[Unit] = ???
   val integer: Parser[Int] = ???
 }
