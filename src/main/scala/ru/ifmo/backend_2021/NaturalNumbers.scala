ef2f272e4fecea2b57f47bd0caab790f3ee3a1ce
Anna Malinina <anna-maks99@lenta.ru>
2021-05-02 22:11:52 +0300

    first

--- src/main/scala/ru/ifmo/backend_2021/NaturalNumbers.scala
+++ src/main/scala/ru/ifmo/backend_2021/NaturalNumbers.scala
@@ -7,6 +7,29 @@ case object Z extends Nat
 case class Succ(s: Nat) extends Nat
 
 object Nat {
-  lazy val natNum: Num[Nat] = ???
-  lazy val natEq: Eq[Nat] = ???
+  lazy val natNum: Num[Nat] = new Num[Nat] {
+    override def +(a: Nat, b: Nat): Nat = (a, b) match {
+      case (Z, Z) => Z
+      case (Z, b: Succ) => Succ(this.+(Z, b.s))
+      case (a: Succ, Z) => Succ(this.+(a.s, Z))
+      case (a: Succ, b: Succ) => Succ(this.+(a.s, b))
+    }
+
+    override def *(a: Nat, b: Nat): Nat = (a, b) match {
+      case (Z, Z) | (Z, _: Succ) | (_: Succ, Z) => Z
+      case (a: Succ, b: Succ) => if (b.s == Z) a else this.+(a, this.*(a, b.s))
+    }
+
+    @scala.annotation.tailrec
+    override def -(a: Nat, b: Nat): Nat = (a, b) match {
+      case (Z, Z) | (Z, _: Succ) => Z
+      case (a: Succ, Z) => a
+      case (a: Succ, b: Succ) => this.-(a.s, b.s)
+    }
+  }
+  lazy val natEq: Eq[Nat] = (a: Nat, b: Nat) => (a, b) match {
+    case (Z, Z) => true
+    case (Z, _: Succ) | (_: Succ, Z) => false
+    case (a: Succ, b: Succ) => eq(a.s, b.s)
+  }
 }
