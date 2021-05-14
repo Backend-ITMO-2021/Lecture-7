28ef2a2c3c22b4262e13ef1c4c07d26622bb53a7
klishin16 <klishin.nd16@gmail.com>
2021-05-14 20:11:44 +0300

    Solve 4 tasks

--- src/main/scala/ru/ifmo/backend_2021/NaturalNumbers.scala
+++ src/main/scala/ru/ifmo/backend_2021/NaturalNumbers.scala
@@ -7,6 +7,32 @@ case object Z extends Nat
 case class Succ(s: Nat) extends Nat
 
 object Nat {
-  lazy val natNum: Num[Nat] = ???
-  lazy val natEq: Eq[Nat] = ???
+  lazy val natNum: Num[Nat] = new Num[Nat] {
+    override def +(a: Nat, b: Nat): Nat = (a, b) match {
+      case (Z, Z) => Z
+      case (Z, b: Succ) => Succ(this.+(Z, b.s))
+      case (a: Succ, Z) => Succ(this.+(a.s, Z))
+      case (a: Succ, b: Succ) => Succ(this.+(a.s, b))
+    }
+
+    override def *(a: Nat, b: Nat): Nat = (a, b) match {
+      case (_, Z) => Z
+      case (Z, _) => Z
+      case (Succ(_), Succ(Z)) => a
+      case (Succ(_), Succ(y)) => natNum.+(natNum.*(a, y), a)
+    }
+
+    override def -(a: Nat, b: Nat): Nat = (a, b) match {
+      case (Z, _) => Z
+      case (Succ(_), Z) => a
+      case (Succ(x), Succ(y)) => natNum.-(x, y)
+    }
+  }
+
+
+  lazy val natEq: Eq[Nat] = (a: Nat, b: Nat) => (a, b) match {
+    case (Z, Z) => true
+    case (Succ(x), Succ(y)) => natEq.eq(x, y)
+    case _ => false
+  }
 }
--- src/main/scala/ru/ifmo/backend_2021/Tree.scala
+++ src/main/scala/ru/ifmo/backend_2021/Tree.scala
@@ -5,15 +5,73 @@ case class Leaf[A]() extends Tree[A]
 case class Node[A](list: NonEmpty[A], left: Tree[A], right: Tree[A]) extends Tree[A]
 
 object Tree {
-  def isEmpty: Tree[Int] => Boolean = ???
-  def contains(value: Int): Tree[Int] => Boolean = ???
-  def insert(value: Int): Tree[Int] => Tree[Int] = ???
-  def delete(value: Int): Tree[Int] => Tree[Int] = ???
-  def fromList: List[Int] => Tree[Int] = ???
+  def isEmpty: Tree[Int] => Boolean = {
+    case Leaf() => true
+    case Node(_, _, _) => false
+  }
+
+  def contains(value: Int): Tree[Int] => Boolean = {
+    case _: Leaf[Int] => false
+    case Node(NonEmpty(head, _), left, right) => head compare value match {
+      case 0 => true
+      case 1 => contains(value)(left)
+      case -1 => contains(value)(right)
+    }
+  }
+
+  def insert(value: Int): Tree[Int] => Tree[Int] = {
+    case Leaf() => Node(NonEmpty(value, Nil), Leaf(), Leaf())
+    case Node(NonEmpty(head, tail), left, right) => head compare value match {
+      case 0 => Node(NonEmpty(head, value :: tail), left, right)
+      case 1 => Node(NonEmpty(head, tail), insert(value)(left), right)
+      case -1 => Node(NonEmpty(head, tail), left, insert(value)(right))
+    }
+  }
+
+  def delete(value: Int): Tree[Int] => Tree[Int] = {
+    case Leaf() => Leaf()
+    case Node(NonEmpty(head, tail), left, right) => head compare value match {
+      case 1 => Node(NonEmpty(head, tail), delete(value)(left), right)
+      case -1 => Node(NonEmpty(head, tail), left, delete(value)(right))
+      case _ => tail match {
+        case _::xt => Node(NonEmpty(head, xt), left, right)
+        case Nil =>
+          def refresh: Tree[Int] => Tree[Int] = {
+            case Node(c, l, r) => Node(c, l, refresh(r))
+            case Leaf() => right
+          }
+
+          refresh(left)
+      }
+    }
+  }
+
+  def fromList(l: List[Int]): Tree[Int] = {
+    val tr: Tree[Int] = Leaf()
+    l.foldLeft(tr)((tree: Tree[Int], value: Int) => insert(value)(tree))
+  }
 
   lazy val foldable = new Foldable[Tree] {
-    def fold[A](fa: Tree[A])(implicit F: Monoid[A]): A = ???
-    def foldMap[A, B](fa: Tree[A])(f: A => B)(implicit F: Monoid[B]): B = ???
-    def foldr[A, B](fa: Tree[A], z: B)(f: A => B => B): B = ???
+    def fold[A](fa: Tree[A])(implicit F: Monoid[A]): A = fa match {
+      case Leaf() => F.zero
+      case Node(list, left, right) => list.head
+    }
+
+    def foldMap[A, B](fa: Tree[A])(f: A => B)(implicit F: Monoid[B]): B = fa match {
+      case Leaf() => F.zero
+      case Node(list, left, right) => F.op(foldMap(left)(f)(F), F.op(
+        NonEmpty.foldable.foldMap(list)(f)(F),
+        foldMap(right)(f)(F)
+      )
+      )
+    }
+
+    def foldr[A, B](fa: Tree[A], z: B)(f: A => B => B): B = fa match {
+      case Leaf() => z
+      case Node(list, left, right) => foldr(
+        left,
+        NonEmpty.foldable.foldr(list, foldr(right, z)(f))(f))(f)
+    }
   }
+
 }
